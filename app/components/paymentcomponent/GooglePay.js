feb6ee0ef4dc69a2a71bf06b1f78296dcb5b5cf6
SultanAhmad1 <sultan@mydigitalpeople.com>
2024-03-01 14:53:52 +0500

    Google pay fix token issue - U

--- app/components/paymentcomponent/GooglePay.js
+++ app/components/paymentcomponent/GooglePay.js
@@ -53,15 +53,16 @@ const GooglePay = (props) => {
 
           console.log("Payment method:",e);
 
-          // const {error: stripeError,paymentIntent} = await stripe.confirmCardPayment(
-          //   clientSecret, 
-          //   {
-          //     payment_method: e.token.id
-          //   }, 
-          //   { 
-          //     handleActions: false 
-          //   }
-          // );
+          const {error: stripeError,paymentIntent} = await stripe.confirmCardPayment(
+            clientSecret, 
+            {
+              payment_method_data:CardElement,
+              payment_method: e.token.card.id
+            }, 
+            { 
+              handleActions: false 
+            }
+          );
 
           console.log("Confirm card payment response from google pay component:", stripeError, "Payment intent:", paymentIntent);
           if (stripeError) {
